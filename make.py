e997d9acefa9ec7a343a917094fe641fa1288003
Alekseev Artem <a.artem060@gmail.com>
2018-08-10 21:18:42 +0300

    add build script

---
+++ make.py
@@ -0,0 +1,39 @@
+import os
+
+CUR_DIR = os.getcwd()
+SOURCE_DIR=CUR_DIR + "/src/"
+BUILD_DIR=CUR_DIR + "/build/"
+CFLAGS="-O0 -g -Wall -Wextra"
+COMPILER="gcc"
+PROGRAMS = {
+    "find_number1" : [
+        "find_number1.c"
+    ],
+    "find_number2" : [
+        "find_number2.c"
+    ],
+    "say_hello" : [
+        "say_hello.c"
+    ],
+    "reverse_array" : [
+        "reverse_array.c",
+        "utility.c",
+        "utility.h"
+    ]
+}
+
+
+def build(program, files):
+    cur_files = [SOURCE_DIR + f for f in files]
+    cmd = "{} {} {} -o {}".format(COMPILER, CFLAGS, " ".join(cur_files), BUILD_DIR + program)
+    os.system(cmd)
+    print(cmd)
+
+
+if not os.path.exists(BUILD_DIR):
+    os.makedirs(BUILD_DIR)
+
+for k, v in PROGRAMS.items():
+    build(k, v)
+
+
